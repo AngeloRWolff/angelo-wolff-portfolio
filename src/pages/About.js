d0da8a6bfa1f3040d9219e9672e06347cb98da42
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-09-25 10:11:17 +0200

    Update About.js
    -Code cleanup
    -Removed uneccessary comments
    -Added code comments

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,10 @@
-// Home.js
 import React, { Component } from 'react';
 
+//component imports
 import GraphItem from '../components/graph-item/GraphItem';
 import FlipCard from '../components/flip-card/FlipCard';
 
+//image imports
 import csIcon from '..//assets/svg/language-icons/csharp.svg';
 import javaIcon from '..//assets/svg/language-icons/java.svg'
 import pyIcon from '..//assets/svg/language-icons/python.svg'
@@ -16,13 +17,10 @@ import cssIcon from '..//assets/svg/language-icons/css.svg'
 class Home extends Component {
   constructor(props, context) {
     super(props, context);
-    this.greetingMessage = this.greetingMessage.bind(this);
-    this.programSkillsMessage = this.programSkillsMessage.bind(this);
   }
   
-  
-  greetingMessage()
-  {
+  //create welcome message to introduce language skillset
+  greetingMessage(){
     return (
     <div style = {{marginTop: "40px"}}>
       <h3 className = "greeting">Hey there, my name is</h3>
@@ -32,7 +30,8 @@ class Home extends Component {
     )
   }
 
-  programSkillsMessage()
+  //create introduction message for language skillset
+  languageSkillsetMessage()
   {
     return (
       <div style = {{marginTop: "40px"}}>
@@ -43,33 +42,11 @@ class Home extends Component {
     )
   }
 
-  /*
-
-{this.greetingMessage()};
-        <div className = "canvas">
-        <LikeBox content = "A"></LikeBox>
-        <LikeBox content = "B"></LikeBox>
-        <LikeBox content = "C"></LikeBox>
-        </div>
-        {this.programSkillsMessage()}
-
-
-         <GraphItem delay = {1250} value = {90}/>
-        <GraphItem delay = {1300} value = {80}/>
-        <GraphItem delay = {1350} value = {70}/>
-        <GraphItem delay = {1400} value = {60}/>
-        <GraphItem delay = {1450} value = {50}/>
-        <GraphItem delay = {1500} value = {40}/>
-        <GraphItem delay = {1550} value = {30}/>
-        <GraphItem delay = {1600} value = {20}/>
-        <GraphItem delay = {1650} value = {10}/>     
-
-
- */
+  //rendering of the about.js page
   render() {
     return (
       <div>
-        These are the programming languages i know
+        {this.languageSkillsetMessage()}
         <div>
         <GraphItem icon = {csIcon}    delay = {0}   value = {95} duration = {1000}  title = 'C#'/>
         <GraphItem icon = {javaIcon}  delay = {100} value = {90} duration = {1000}  title = 'Java'/>
@@ -79,11 +56,7 @@ class Home extends Component {
         <GraphItem icon = {htmlIcon}  delay = {500} value = {45} duration = {1000}  title = 'HTML'/>
         <GraphItem icon = {cssIcon}   delay = {600} value = {40} duration = {1000}  title = 'CSS'/>   
         </div>
-
-        <FlipCard image = {null} title = {null} list = {null} />
- 
-        
-        
+        <FlipCard image = {null} title = {null} list = {null} />      
       </div>       
     );
   }
