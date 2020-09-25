af5ace376f7887a42cd628f7f9128f11c2312a9b
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-09-25 10:03:09 +0200

    Update App.js
    -Code Cleanup/Formatting

--- src/App.js
+++ src/App.js
@@ -6,29 +6,21 @@ import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 
-
 function App() {
   return (
-
-
     <div className="App">
       <Router>
-        
-
-        <div>
-      
+        <div>   
           <ul className="App-header Fadein-top">           
             <Link to={'/about'} className="Fadein-top"> About</Link>           
             <Link to={'/projects'} className="Fadein-top">Projects</Link>
             <Link to={'/contact'} className="Fadein-top">Contact </Link>
           </ul>
-
           <Switch>
             <Route exact path='/' component={Idle} /> 
             <Route path='/about' component={About} />
             <Route path='/projects' component={Projects} />
-            <Route path='/contact' component={Contact}/>
-         
+            <Route path='/contact' component={Contact}/>       
           </Switch>
         </div>
       </Router>
