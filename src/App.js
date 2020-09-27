670de847455f50ae373a56c29a4cc03b222d619b
AngeloRWolff <45687159+AngeloRWolff@users.noreply.github.com>
2020-09-27 17:11:03 +0200

    Routing Issue T4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { BrowserRouter as Router,Switch, Route, Link } from 'react-router-dom';
 import Idle from './pages/Idle.js';
 import About from './pages/About';
@@ -9,19 +10,24 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
+    <div>
       <Router>
-      <ul className="App-header Fadein-top">  
-            <Link to={'/about'} className="Fadein-top"> About</Link>           
-            <Link to={'/projects'} className="Fadein-top">Projects</Link>
-            <Link to={'/contact'} className="Fadein-top">Contact </Link>
-          </ul>
-          <Switch>      
-            <Route exact path='/' component={Idle} /> 
-            <Route path='/about' component={About} />
-            <Route path='/projects' component={Projects} />
-            <Route path='/contact' component={Contact}/>               
-          </Switch>
-      </Router>
+        <Route exact path='/' component={Idle} />
+        <Route path='/about' component={About} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/contact' component={Contact} />
+   
+      <NavItem>
+      <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+      </NavItem>
+     <NavItem>
+      <NavLink tag={Link} className="text-dark" to="/about">Counter</NavLink>
+     </NavItem>
+     <NavItem>
+      <NavLink tag={Link} className="text-dark" to="/projects">Fetch data</NavLink>
+      </NavItem>
+     </Router>
+    </div>
     )
   }
   
